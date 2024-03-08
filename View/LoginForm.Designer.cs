b6a6d19a4ff61576943c6b3d8c30041f5e223e8b
Benjamin Lively <blively2@my.westga.edu>
2024-03-08 13:19:06 -0500

    core functionality for LoginForm

--- View/LoginForm.Designer.cs
+++ View/LoginForm.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.LoginWarningLabel = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.WelcomeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.UsernameTextBox.Location = new System.Drawing.Point(304, 147);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(200, 32);
+            this.UsernameTextBox.TabIndex = 0;
+            this.UsernameTextBox.TextChanged += new System.EventHandler(this.UsernameOrPasswordTextBoxTextChanged);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.PasswordTextBox.Location = new System.Drawing.Point(304, 205);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(200, 32);
+            this.PasswordTextBox.TabIndex = 1;
+            this.PasswordTextBox.TextChanged += new System.EventHandler(this.UsernameOrPasswordTextBoxTextChanged);
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.UsernameLabel.Location = new System.Drawing.Point(152, 150);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(119, 26);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username:";
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.PasswordLabel.Location = new System.Drawing.Point(157, 208);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(114, 26);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password:";
+            // 
+            // LoginWarningLabel
+            // 
+            this.LoginWarningLabel.AutoSize = true;
+            this.LoginWarningLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.LoginWarningLabel.ForeColor = System.Drawing.Color.Red;
+            this.LoginWarningLabel.Location = new System.Drawing.Point(510, 147);
+            this.LoginWarningLabel.Name = "LoginWarningLabel";
+            this.LoginWarningLabel.Size = new System.Drawing.Size(262, 24);
+            this.LoginWarningLabel.TabIndex = 4;
+            this.LoginWarningLabel.Text = "Incorrect Username/Password";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.LoginButton.Location = new System.Drawing.Point(304, 267);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(200, 35);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButtonClick);
+            // 
+            // WelcomeLabel
+            // 
+            this.WelcomeLabel.AutoSize = true;
+            this.WelcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.WelcomeLabel.Location = new System.Drawing.Point(28, 53);
+            this.WelcomeLabel.Name = "WelcomeLabel";
+            this.WelcomeLabel.Size = new System.Drawing.Size(733, 29);
+            this.WelcomeLabel.TabIndex = 6;
+            this.WelcomeLabel.Text = "Enter your credentials to access our Furniture Management System.";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.WelcomeLabel);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.LoginWarningLabel);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sofa So Good FMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Label LoginWarningLabel;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label WelcomeLabel;
     }
 }
 
--- View/LoginForm.cs
+++ View/LoginForm.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace SofaSoGood
 {
+    /// <summary>
+    /// Login form that handles user authentication.
+    /// </summary>
     public partial class LoginForm : Form
     {
+        /// <summary>
+        /// Constructs the LoginForm and initializes its components.
+        /// </summary>
         public LoginForm()
         {
             InitializeComponent();
+            this.LoginWarningLabel.Text = string.Empty;
         }
+
+        /// <summary>
+        /// Validates user input against predefined credentials on login button click.
+        /// If validation is successful, hides the login form and shows the main dashboard.
+        /// Otherwise, displays an error message.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Contains the event data.</param>
+        private void LoginButtonClick(object sender, EventArgs e)
+        {
+            if (this.UsernameTextBox.Text == "employee" && this.PasswordTextBox.Text == "test1234")
+            {
+                //Do something to indicate login success and show the next form
+                System.Diagnostics.Debug.WriteLine("Login Successful");
+            }
+            else
+            {
+                this.UsernameTextBox.Text = string.Empty;
+                this.PasswordTextBox.Text = string.Empty;
+                this.LoginWarningLabel.Text = "Incorrect Username/Password";
+            }
+
+        }
+
+        /// <summary>
+        /// Clears the error message when the user begins typing their UserName or Password again.
+        /// </summary>
+        private void UsernameOrPasswordTextBoxTextChanged(object sender, EventArgs e)
+        {
+            this.LoginWarningLabel.Text = string.Empty;
+        }
+
     }
 }
