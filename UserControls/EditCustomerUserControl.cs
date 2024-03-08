8e332d80adce441c1f88dd444721c03c958a957d
Benjamin Lively <blively2@my.westga.edu>
2024-03-08 16:34:20 -0500

    Comments for UserControls

--- UserControls/EditCustomerUserControl.cs
+++ UserControls/EditCustomerUserControl.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace SofaSoGood.UserControls
 {
+    /// <summary>
+    /// UserControl for Editing Customers in the database.
+    /// Has an CustomerController instance to interact with the DB.
+    /// </summary>
     public partial class EditCustomerUserControl : UserControl
     {
+        /// <summary>
+        /// Constructor for EditCustomerUserControl.
+        /// Initializes the controller and the component for the UI.
+        /// </summary>
         public EditCustomerUserControl()
         {
             InitializeComponent();
--- UserControls/RegisterCustomerUserControl.cs
+++ UserControls/RegisterCustomerUserControl.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace SofaSoGood
 {
+    /// <summary>
+    /// UserControl for Registering Customers in the database.
+    /// Has an CustomerController instance to interact with the DB.
+    /// </summary>
     public partial class RegisterCustomerUserControl : UserControl
     {
+        /// <summary>
+        /// Constructor for RegisterCustomerUserControl.
+        /// Initializes the controller and the component for the UI.
+        /// </summary>
         public RegisterCustomerUserControl()
         {
             InitializeComponent();
