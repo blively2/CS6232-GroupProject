89679024e123761585aa6867ba8c7b613341f865
Stan Shadrix <gshadrix@westga.edu>
2024-03-10 18:42:02 -0400

    added member model class

---
+++ model/Member.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofaSoGood.Model
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string ContactPhone { get; set; }
+
+    }
+}
