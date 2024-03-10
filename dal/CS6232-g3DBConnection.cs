bdb7b3842504e3d7a1ddfb2baee9838e7482c07c
Stan Shadrix <gshadrix@westga.edu>
2024-03-10 18:41:24 -0400

    added DBConnection file to DAL

---
+++ dal/CS6232-g3DBConnection.cs
@@ -0,0 +1,19 @@
+using System.Data.SqlClient;
+
+namespace SofaSoGood.DAL
+{
+    /// <summary>
+    /// Provides a way to manage database connections for the SofaSoGood application.
+    /// </summary>
+    public static class SofaSoGoodDBConnection
+    {
+        /// <summary>
+        /// Creates and returns a new instance of SqlConnection configured for the SofaSoGood database.
+        /// </summary>
+        public static SqlConnection GetConnection()
+        {
+            string connectionString = "Data Source=localhost;Initial Catalog=CS6232-g3;Integrated Security=True";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
