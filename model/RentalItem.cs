812912307ba17e3c1cf53beadc42789c70a3b341
Stan Shadrix <gshadrix@westga.edu>
2024-04-02 08:56:51 -0400

    added RentalItem model class

---
+++ model/RentalItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SofaSoGood.Model
+{
+    public class RentalItem
+    {
+        /// <summary>
+        /// Gets or sets the rental item identifier.
+        /// </summary>
+        public int RentalItemID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rental transaction identifier.
+        /// </summary>
+        public int RentalTransactionID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the furniture identifier.
+        /// </summary>
+        public int FurnitureID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quantity.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the daily rate.
+        /// </summary>
+        public decimal DailyRate { get; set; }
+    }
+}
