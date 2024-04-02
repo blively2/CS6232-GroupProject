6e5b9e3cb6b9db3f4b9a152f0d5f7e94502bc432
Stan Shadrix <gshadrix@westga.edu>
2024-04-02 13:54:34 -0400

    added rentalController class

---
+++ controller/RentalController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using SofaSoGood.DAL;
+using SofaSoGood.Model;
+
+namespace SofaSoGood.Controller
+{
+    public class RentalController
+    {
+        private readonly RentalDAL rentalDAL;
+        private readonly FurnitureDAL furnitureDAL;
+
+        public RentalController()
+        {
+            rentalDAL = new RentalDAL();
+            furnitureDAL = new FurnitureDAL();
+        }
+
+        public void CreateRentalTransaction(RentalTransaction rentalTransaction)
+        {
+            rentalDAL.CreateRentalTransaction(rentalTransaction);
+
+            foreach (var item in rentalTransaction.RentalItems)
+            {
+                furnitureDAL.UpdateStockQuantity(item.FurnitureID, item.Quantity);
+            }
+        }
+
+        public RentalTransaction GetRentalTransaction(int rentalTransactionId)
+        {
+            return rentalDAL.GetRentalTransaction(rentalTransactionId);
+        }
+
+        public List<Furniture> GetAvailableFurniture()
+        {
+            return furnitureDAL.GetAvailableFurniture();
+        }
+
+        public decimal GetFurnitureDailyRate(int furnitureId)
+        {
+            return furnitureDAL.GetFurnitureDailyRate(furnitureId);
+        }
+
+        public int GetNextRentalTransactionID()
+        {
+            return rentalDAL.GetNextRentalTransactionID();
+        }
+    }
+}
