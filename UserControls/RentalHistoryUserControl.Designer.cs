2e552a96dc9b26e433880ab2bef80e99824c8891
Stan Shadrix <gshadrix@westga.edu>
2024-04-06 17:02:28 -0400

    added RentalHistoryUserControl and necessary elements

---
+++ UserControls/RentalHistoryUserControl.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SofaSoGood.UserControls
+{
+    partial class RentalHistoryUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rentalHistoryDataGridView = new System.Windows.Forms.DataGridView();
+            this.memberIdTextBox = new System.Windows.Forms.TextBox();
+            this.rentalHistoryTitleLabel = new System.Windows.Forms.Label();
+            this.memberIdLabel = new System.Windows.Forms.Label();
+            this.searchHistoryButton = new System.Windows.Forms.Button();
+            this.clearSearchButton = new System.Windows.Forms.Button();
+            this.errorMessageLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rentalHistoryDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rentalHistoryDataGridView
+            // 
+            this.rentalHistoryDataGridView.AllowUserToAddRows = false;
+            this.rentalHistoryDataGridView.AllowUserToDeleteRows = false;
+            this.rentalHistoryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.rentalHistoryDataGridView.Location = new System.Drawing.Point(3, 73);
+            this.rentalHistoryDataGridView.Name = "rentalHistoryDataGridView";
+            this.rentalHistoryDataGridView.ReadOnly = true;
+            this.rentalHistoryDataGridView.Size = new System.Drawing.Size(638, 197);
+            this.rentalHistoryDataGridView.TabIndex = 0;
+            // 
+            // memberIdTextBox
+            // 
+            this.memberIdTextBox.Location = new System.Drawing.Point(119, 42);
+            this.memberIdTextBox.Name = "memberIdTextBox";
+            this.memberIdTextBox.Size = new System.Drawing.Size(100, 20);
+            this.memberIdTextBox.TabIndex = 1;
+            // 
+            // rentalHistoryTitleLabel
+            // 
+            this.rentalHistoryTitleLabel.AutoSize = true;
+            this.rentalHistoryTitleLabel.Location = new System.Drawing.Point(257, 13);
+            this.rentalHistoryTitleLabel.Name = "rentalHistoryTitleLabel";
+            this.rentalHistoryTitleLabel.Size = new System.Drawing.Size(99, 13);
+            this.rentalHistoryTitleLabel.TabIndex = 2;
+            this.rentalHistoryTitleLabel.Text = "View Rental History";
+            // 
+            // memberIdLabel
+            // 
+            this.memberIdLabel.AutoSize = true;
+            this.memberIdLabel.Location = new System.Drawing.Point(51, 47);
+            this.memberIdLabel.Name = "memberIdLabel";
+            this.memberIdLabel.Size = new System.Drawing.Size(62, 13);
+            this.memberIdLabel.TabIndex = 3;
+            this.memberIdLabel.Text = "Member ID:";
+            // 
+            // searchHistoryButton
+            // 
+            this.searchHistoryButton.Location = new System.Drawing.Point(225, 39);
+            this.searchHistoryButton.Name = "searchHistoryButton";
+            this.searchHistoryButton.Size = new System.Drawing.Size(75, 23);
+            this.searchHistoryButton.TabIndex = 4;
+            this.searchHistoryButton.Text = "Search";
+            this.searchHistoryButton.UseVisualStyleBackColor = true;
+            this.searchHistoryButton.Click += new System.EventHandler(this.searchHistoryButton_Click);
+            // 
+            // clearSearchButton
+            // 
+            this.clearSearchButton.Location = new System.Drawing.Point(306, 39);
+            this.clearSearchButton.Name = "clearSearchButton";
+            this.clearSearchButton.Size = new System.Drawing.Size(75, 23);
+            this.clearSearchButton.TabIndex = 5;
+            this.clearSearchButton.Text = "Clear";
+            this.clearSearchButton.UseVisualStyleBackColor = true;
+            this.clearSearchButton.Click += new System.EventHandler(this.clearSearchButton_Click);
+            // 
+            // errorMessageLabel
+            // 
+            this.errorMessageLabel.AutoSize = true;
+            this.errorMessageLabel.Location = new System.Drawing.Point(387, 44);
+            this.errorMessageLabel.Name = "errorMessageLabel";
+            this.errorMessageLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorMessageLabel.TabIndex = 6;
+            // 
+            // RentalHistoryUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.errorMessageLabel);
+            this.Controls.Add(this.clearSearchButton);
+            this.Controls.Add(this.searchHistoryButton);
+            this.Controls.Add(this.memberIdLabel);
+            this.Controls.Add(this.rentalHistoryTitleLabel);
+            this.Controls.Add(this.memberIdTextBox);
+            this.Controls.Add(this.rentalHistoryDataGridView);
+            this.Name = "RentalHistoryUserControl";
+            this.Size = new System.Drawing.Size(644, 277);
+            ((System.ComponentModel.ISupportInitialize)(this.rentalHistoryDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView rentalHistoryDataGridView;
+        private System.Windows.Forms.TextBox memberIdTextBox;
+        private System.Windows.Forms.Label rentalHistoryTitleLabel;
+        private System.Windows.Forms.Label memberIdLabel;
+        private System.Windows.Forms.Button searchHistoryButton;
+        private System.Windows.Forms.Button clearSearchButton;
+        private System.Windows.Forms.Label errorMessageLabel;
+    }
+}
---
+++ UserControls/RentalHistoryUserControl.cs
@@ -0,0 +1,88 @@
+﻿using SofaSoGood.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SofaSoGood.UserControls
+{
+    public partial class RentalHistoryUserControl : UserControl
+    {
+        private RentalController rentalController;
+
+        public RentalHistoryUserControl()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            rentalController = new RentalController();
+        }
+
+        private void InitializeDataGridView()
+        {
+            rentalHistoryDataGridView.Columns.Add("RentalTransactionID", "Transaction ID");
+            rentalHistoryDataGridView.Columns.Add("RentalDate", "Rental Date");
+            rentalHistoryDataGridView.Columns.Add("DueDate", "Due Date");
+            rentalHistoryDataGridView.Columns.Add("TotalCost", "Total Cost");
+
+            rentalHistoryDataGridView.Columns["RentalDate"].DefaultCellStyle.Format = "yyyy-MM-dd";
+            rentalHistoryDataGridView.Columns["DueDate"].DefaultCellStyle.Format = "yyyy-MM-dd";
+            rentalHistoryDataGridView.Columns["TotalCost"].DefaultCellStyle.Format = "C2";
+
+            rentalHistoryDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        public void LoadRentalHistory(int memberId)
+        {
+            rentalHistoryDataGridView.Rows.Clear();
+
+            var rentalHistory = rentalController.GetRentalHistoryByMemberId(memberId);
+
+            if (rentalHistory.Count > 0)
+            {
+                foreach (var transaction in rentalHistory)
+                {
+                    rentalHistoryDataGridView.Rows.Add(transaction.RentalTransactionID,
+                                                   transaction.RentalDate.ToString("yyyy-MM-dd"),
+                                                   transaction.DueDate.ToString("yyyy-MM-dd"),
+                                                   transaction.TotalCost.ToString("C2"));
+                }
+            }
+            else
+            {
+                MessageBox.Show($"No rental history found for Member ID: {memberId}", "Rental History Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void searchHistoryButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(memberIdTextBox.Text, out int memberId))
+            {
+                LoadRentalHistory(memberId);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Member ID.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                rentalHistoryDataGridView.Rows.Clear();
+            }
+        }
+
+        private void clearSearchButton_Click(object sender, EventArgs e)
+        {
+            ClearHistoryForm();
+        }
+
+        private void ClearHistoryForm()
+        {
+            rentalHistoryDataGridView.Rows.Clear();
+
+            memberIdTextBox.Clear();
+
+            errorMessageLabel.Text = "";
+        }
+    }
+}
