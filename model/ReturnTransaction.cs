d8fdef10ce67862dfbf84db4a3fcd4dc3f653f27
Stan Shadrix <gshadrix@westga.edu>
2024-04-18 19:39:15 -0400

    added ReturnTransaction model class

---
+++ model/ReturnTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofaSoGood.Model
+{
+    public class ReturnTransaction
+    {
+        public int ReturnTransactionID { get; set; }
+        public int EmployeeID { get; set; }
+        public int MemberID { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public decimal ReturnAmount { get; set; }
+        public decimal FineAmount { get; set; }
+        public List<ReturnItem> ReturnItems { get; set; } = new List<ReturnItem>();
+
+    }
+}
