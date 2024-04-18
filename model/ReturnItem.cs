76f4c0102919da2056e91ec5c9c1432d80ddb65f
Stan Shadrix <gshadrix@westga.edu>
2024-04-18 19:38:34 -0400

    added ReturnItem model class

---
+++ model/ReturnItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofaSoGood.Model
+{
+    public class ReturnItem
+    {
+        public int ReturnItemID { get; set; }
+        public int ReturnTransactionID { get; set; }
+        public int RentalItemID { get; set; }
+        public int QuantityReturned { get; set; }
+    }
+}
