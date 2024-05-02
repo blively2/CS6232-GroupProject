361215b6564524ad2e4062ac977ef3552f4368ab
Stan Shadrix <gshadrix@westga.edu>
2024-05-02 13:34:43 -0400

    updated method name used in rentFurnitureUserControl based on changes

--- UserControlsMember/RentFurnitureUserControl.cs
+++ UserControlsMember/RentFurnitureUserControl.cs
@@ -201,7 +201,7 @@ namespace SofaSoGood.UserControls
 
 
 
-            this.rentalController.CreateRentalTransactionWithInventoryUpdate(rentalTransaction);
+            this.rentalController.CreateRentalTransaction(rentalTransaction);
             var receiptForm = new ReceiptForm(rentalTransaction);
             receiptForm.ShowDialog();
             this.SelectedFurnitureDataGridView.Rows.Clear();
